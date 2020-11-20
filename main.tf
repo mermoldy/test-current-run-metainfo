7a89574e87d1fbe84d139ad254896972c413edf0
Serhii Babak <s.babak@scalr.com>
2020-11-20 15:39:14 +0200

    wip

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
-required_providers {
-  scalr = {
-    source  = "scalr.com/scalr/scalr"
-    version = "1.0.0"
+
+terraform {
+  required_providers {
+    scalr = {
+      source  = "scalr.com/scalr/scalr"
+      version = "1.0.0"
+    }
   }
 }
-
 # resource "null_resource" "trigger" {
 #   triggers = {
 #     always_run = "${timestamp()}"
